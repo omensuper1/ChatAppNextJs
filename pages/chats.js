3dfcc7f6049c131bcab59f6ba9729dd470a97cd6
omensuper1 <136446205+omensuper1@users.noreply.github.com>
2023-12-09 14:25:35 +0700

    Add files via upload

---
+++ pages/chats.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, useContext } from "react";
+
+import { Context } from "../context";
+
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+
+const ChatEngine = dynamic(() =>
+  import("react-chat-engine").then((module) => module.ChatEngine)
+);
+const MessageFormSocial = dynamic(() =>
+  import("react-chat-engine").then((module) => module.MessageFormSocial)
+);
+
+export default function Home() {
+  const { username, secret } = useContext(Context);
+  const [showChat, setShowChat] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof document !== undefined) {
+      setShowChat(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (username === "" || secret === "") {
+      router.push("/");
+    }
+  }, [username, secret]);
+
+  if (!showChat) return <div />;
+
+  return (
+    <div className="background">
+      <div className="shadow">
+        <ChatEngine
+          height="calc(100vh - 212px)"
+          projectID="8c90ca83-11c2-4a8b-a9bc-c8de49deac32"
+          userName={username}
+          userSecret={secret}
+          renderNewMessageForm={() => <MessageFormSocial />}
+        />
+      </div>
+    </div>
+  );
+}
